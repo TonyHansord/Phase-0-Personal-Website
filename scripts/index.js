958c9bbd177f891e0be09a89917e333fcc175faa
Anthony Hansord <tonyhansorddev@gmail.com>
2022-10-25 22:33:34 +1100

    Sorted out grid

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,7 @@
 const displayMenuBtn = document.querySelector(".toggle-on");
-const hideMenuBtn = document.querySelector(".toggle-off");
 const menuContent = document.querySelector(".dropdown-content");
 const menuClasses = menuContent.classList;
 
 displayMenuBtn.addEventListener("click", function () {
   menuClasses.add("toggle");
-});
-
-hideMenuBtn.addEventListener("click", function () {
-  menuClasses.remove("toggle");
 });
