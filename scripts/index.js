7934d5453ccf44a9ca1f1e1609dd6d2acbb24bad
TonyHansord <59306359+TonyHansord@users.noreply.github.com>
2022-10-27 18:23:24 +1100

    creating object for data

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,16 @@ const displayMenuBtn = document.querySelector(".toggle-on");
 const menuContent = document.querySelector(".dropdown-content");
 const menuClasses = menuContent.classList;
 
+const projectData = {
+
+projects: [
+{
+
+}
+}
+
+}
+
 displayMenuBtn.addEventListener("click", function () {
   
   menuClasses.contains("toggle") ? menuClasses.remove ("toggle") : menuClasses.add("toggle");
