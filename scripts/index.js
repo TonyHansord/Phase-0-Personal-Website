2ea9ba3d37ce3bd349ab1caf32b1846b7cd33294
Anthony Hansord <tonyhansorddev@gmail.com>
2023-08-05 09:53:08 +1000

    Added extra projects and removed form

--- scripts/index.js
+++ scripts/index.js
@@ -1,93 +1,107 @@
 const body = document.body;
-const main = document.querySelector("main");
-const menuLinks = document.querySelectorAll(".menu-link");
-const displayMenuBtn = document.querySelector(".toggle");
-const menuContent = document.getElementById("nav-modal");
+const main = document.querySelector('main');
+const menuLinks = document.querySelectorAll('.menu-link');
+const displayMenuBtn = document.querySelector('.toggle');
+const menuContent = document.getElementById('nav-modal');
 const menuClasses = menuContent.classList;
 
-const fProjectTitle = document.querySelectorAll(".fproject-title");
-const fProjectDesc = document.querySelectorAll(".fproject-desc");
-const fpTechList = document.querySelectorAll(".fptech-list");
-const fRepo = document.querySelectorAll(".frepo");
-const fProjectLink = document.querySelectorAll(".flive-site");
-const projectType = document.querySelectorAll(".project-type");
-const projectTitle = document.querySelectorAll(".project-title");
-const projectDesc = document.querySelectorAll(".project-desc");
-const techList = document.querySelectorAll(".tech-list");
-const projectRepo = document.querySelectorAll(".repo");
-const projectLink = document.querySelectorAll(".live-site");
+const fProjectTitle = document.querySelectorAll('.fproject-title');
+const fProjectDesc = document.querySelectorAll('.fproject-desc');
+const fpTechList = document.querySelectorAll('.fptech-list');
+const fRepo = document.querySelectorAll('.frepo');
+const fProjectLink = document.querySelectorAll('.flive-site');
+const projectType = document.querySelectorAll('.project-type');
+const projectTitle = document.querySelectorAll('.project-title');
+const projectDesc = document.querySelectorAll('.project-desc');
+const techList = document.querySelectorAll('.tech-list');
+const projectRepo = document.querySelectorAll('.repo');
+const projectLink = document.querySelectorAll('.live-site');
 
 // Data for card objects, will put in a json file further down the track
 const projectData = {
   featuredProjects: [
     {
-      title: "BookLogue: Reading Manager",
-      desc: "An iOS app that allows users to track their reading progress and add notes about the books they read.",
-      tech: ["Swift", "Xcode", "Cocoapods", "Firebase"],
-      link: "https://apps.apple.com/us/app/booklogue/id1521788378?mt=8",
+      title: 'BookLogue: Reading Manager',
+      desc: 'An iOS app that allows users to track their reading progress and add notes about the books they read.',
+      tech: ['Swift', 'Xcode', 'Cocoapods', 'Firebase'],
+      link: 'https://apps.apple.com/us/app/booklogue/id1521788378?mt=8',
     },
     {
-      title: "Landing Page for Booklogue",
-      desc: "Website for the aforementioned Booklogue App, using Bootstrap and a 3rd party form handler.",
-      tech: ["HTML", "CSS", "Bootstrap"],
-      repo: "https://github.com/TonyHansord/BookLogueWeb",
-      link: "https://www.booklogue.com",
+      title: 'Landing Page for Booklogue',
+      desc: 'Website for the aforementioned Booklogue App, using Bootstrap and a 3rd party form handler.',
+      tech: ['HTML', 'CSS', 'Bootstrap'],
+      repo: 'https://github.com/TonyHansord/BookLogueWeb',
+      link: 'https://www.booklogue.com',
+    },
+    {
+      title: 'Find that Show',
+      desc: 'App front end made with HTML, CSS and JS, as part of software engineering bootcamp. User can search for TV shows, and view details about each show. Fetches data from the TV Maze API.',
+      tech: ['HTML', 'CSS', 'Javascript'],
+      repo: 'https://github.com/TonyHansord/Phase-1-Project',
+      link: 'https://findthatshow.netlify.app',
+    },
+    {
+      title: 'Game collection app',
+      desc: 'Proof of concept web app front-end made with React, as part of software engineering bootcamp. User can add games to their collection, and view details about each game.',
+      tech: ['HTML', 'CSS', 'React', 'Javascript'],
+      repo: 'https://github.com/TonyHansord/phase-2-game-collectors-app',
+      link: 'https://game-collect.netlify.app',
     },
   ],
   otherProjects: [
     {
-      title: "NFT preview card component",
-      type: "Frontend Mentor Challenge",
+      title: 'NFT preview card component',
+      type: 'Frontend Mentor Challenge',
       description:
-        "A component that displays a preview of an NFT card that would be used on a NFT Marketplace site.",
-      tech: ["HTML", "CSS", "Bootstrap"],
-      repo: "https://github.com/TonyHansord/FM-Challenges/tree/master/NFT-element",
-      liveSite: "https://nft-element-tonyhansord.vercel.app",
+        'A component that displays a preview of an NFT card that would be used on a NFT Marketplace site.',
+      tech: ['HTML', 'CSS', 'Bootstrap'],
+      repo: 'https://github.com/TonyHansord/FM-Challenges/tree/master/NFT-element',
+      liveSite: 'https://nft-element-tonyhansord.vercel.app',
     },
     {
-      title: "Product preview card component",
-      type: "Frontend Mentor Challenge",
+      title: 'Product preview card component',
+      type: 'Frontend Mentor Challenge',
       description:
-        "Another challenge involving creating a card component, this one a product preview card.",
-      tech: ["HTML", "CSS"],
-      repo: "https://github.com/TonyHansord/FrontEndMentorChallenges/tree/master/product-preview-card-component",
-      liveSite: "https://product-preview-card-tonyhansord.vercel.app/",
+        'Another challenge involving creating a card component, this one a product preview card.',
+      tech: ['HTML', 'CSS'],
+      repo: 'https://github.com/TonyHansord/FrontEndMentorChallenges/tree/master/product-preview-card-component',
+      liveSite: 'https://product-preview-card-tonyhansord.vercel.app/',
     },
     {
-      title: "4 Card Feature Section",
-      type: "Frontend Mentor Challenge",
+      title: '4 Card Feature Section',
+      type: 'Frontend Mentor Challenge',
       description:
-        "This challenge was to recreate a four card section, and to practice responsive layouts.",
-      tech: ["HTML", "CSS", "Figma"],
-      repo: "https://github.com/TonyHansord/FM-Challenges/tree/master/four-card-feature-section",
-      liveSite: "https://four-card-section-tonyhansord.vercel.app",
+        'This challenge was to recreate a four card section, and to practice responsive layouts.',
+      tech: ['HTML', 'CSS', 'Figma'],
+      repo: 'https://github.com/TonyHansord/FM-Challenges/tree/master/four-card-feature-section',
+      liveSite: 'https://four-card-section-tonyhansord.vercel.app',
     },
     {
-      title: "Time tracking dashboard",
-      type: "Frontend Mentor Challenge",
+      title: 'Time tracking dashboard',
+      type: 'Frontend Mentor Challenge',
       description:
-        "This challenge was to recreate a dashboard for a time-tracking app.",
-      tech: ["HTML", "CSS", "Javascript"],
-      repo: "https://github.com/TonyHansord/FM-Challenges/tree/time-tracker/time-tracking-dashboard",
-      liveSite: "https://tonyhansord-time-tracker.netlify.app",
+        'This challenge was to recreate a dashboard for a time-tracking app.',
+      tech: ['HTML', 'CSS', 'Javascript'],
+      repo: 'https://github.com/TonyHansord/FM-Challenges/tree/time-tracker/time-tracking-dashboard',
+      liveSite: 'https://tonyhansord-time-tracker.netlify.app',
     },
     {
-      title: "Skilled e-learning landing page",
-      type: "Frontend Mentor Challenge",
+      title: 'Skilled e-learning landing page',
+      type: 'Frontend Mentor Challenge',
       description:
-        "This challenge was to recreate a landing page, with a focus on making it responsive.",
-      tech: ["HTML", "CSS", "Sass"],
-      repo: "https://github.com/TonyHansord/FrontEndMentorChallenges/tree/master/skilled-elearning-landing-page",
-      liveSite: "https://tonyhansord-elearning-landing-page.netlify.app",
+        'This challenge was to recreate a landing page, with a focus on making it responsive.',
+      tech: ['HTML', 'CSS', 'Sass'],
+      repo: 'https://github.com/TonyHansord/FrontEndMentorChallenges/tree/master/skilled-elearning-landing-page',
+      liveSite: 'https://tonyhansord-elearning-landing-page.netlify.app',
     },
     {
-      title: "Wedding Website",
-      type: "Other",
+      title: 'Wedding Website',
+      type: 'Other',
       description:
         "A website I built with GoDaddy Website builder, for my wife and I's wedding.",
-      tech: ["GoDaddy"],
-      repo: "",
-      liveSite: "https://www.katntony.com",
+      tech: ['GoDaddy'],
+      repo: '',
+      liveSite: 'https://www.katntony.com',
     },
   ],
 };
@@ -117,40 +131,40 @@ for (let i = 0; i < projectType.length; i++) {
 }
 
 function openMenu() {
-  displayMenuBtn.classList.add("open");
-  menuClasses.remove("hidden");
-  body.style.overflow = "hidden";
-  main.classList.add("is-blurred");
+  displayMenuBtn.classList.add('open');
+  menuClasses.remove('hidden');
+  body.style.overflow = 'hidden';
+  main.classList.add('is-blurred');
 }
 
 function closeMenu() {
-  displayMenuBtn.classList.remove("open");
-  menuClasses.add("hidden");
-  body.style.overflow = "auto";
-  main.classList.remove("is-blurred");
+  displayMenuBtn.classList.remove('open');
+  menuClasses.add('hidden');
+  body.style.overflow = 'auto';
+  main.classList.remove('is-blurred');
 }
 
-main.addEventListener("click", function () {
-  if (!menuClasses.contains("hidden")) {
+main.addEventListener('click', function () {
+  if (!menuClasses.contains('hidden')) {
     closeMenu();
   }
 });
 
 menuLinks.forEach((element) => {
   console.log(element);
-  element.addEventListener("click", closeMenu);
+  element.addEventListener('click', closeMenu);
 });
 
-displayMenuBtn.addEventListener("click", function () {
-  if (!menuClasses.contains("transition")) {
-    menuClasses.add("transition");
+displayMenuBtn.addEventListener('click', function () {
+  if (!menuClasses.contains('transition')) {
+    menuClasses.add('transition');
   }
-  menuClasses.contains("hidden") ? openMenu() : closeMenu();
+  menuClasses.contains('hidden') ? openMenu() : closeMenu();
 });
 
 // prevents menu from showing up on viewport resize
-window.addEventListener("resize", function () {
-  if (menuClasses.contains("transition")) {
-    menuClasses.remove("transition");
+window.addEventListener('resize', function () {
+  if (menuClasses.contains('transition')) {
+    menuClasses.remove('transition');
   }
 });
